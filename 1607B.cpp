c754f8aad0ac41aaa6d0df31e751c7153445a852
Ashish Kashyap <akk312000@gmail.com>
2021-11-21 12:21:29 +0530

    added hello message

--- 1607B.cpp
+++ 1607B.cpp
@@ -66,16 +66,10 @@ ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n
 
 void solve() {
 
-	int t; cin >> t;
-	while (t--)
-	{
-		int n; cin >> n;
-		vector<int>arr(n);
-		for (int i = 0; i < n; i++)
-		{
-			cin >> arr[i];
-		}
-	}
+	cout << "Hi";
+
+
+	cout << "yo";
 
 
 
